cc1d1321ac1ff2da4942ae1bfb1e892dabdcd485
Joana Palero <joana.palero24@gmail.com>
2018-07-01 00:48:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    part = filename.split(" - ")
+    parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
     
